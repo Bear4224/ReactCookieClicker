9e86d7e5266dda0f9e53f6d8badb7e44f4f42b2a
Jacob Showman <jacob.showman@mrjaffesclass.com>
2023-02-01 12:47:42 -0800

    Finishd, only the font doesn't work entirely

--- cookieclicker/src/index.js
+++ cookieclicker/src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import cookie from './cookie.png'
 import reportWebVitals from './reportWebVitals';
+//To run, make sure to do npm install in the cookieclicker
+//directory, then npm start to run it.
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+
+  handleClick(){
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  render() {
+    const count = this.state.count;
+    return (
+      <div className="App">
+      <header className="App-header">
+        <br></br>
+        <br></br>
+        <button className="cookieButton" onClick={() => this.handleClick()}><img src={cookie} className="App-logo" alt="logo" /></button>
+        <br></br>
+        <br></br>
+        <p class="p1">
+          Click me ...
+        </p>
+        <p class="p1">
+          {this.state.count}
+        </p>
+      </header>
+    </div>
+    );
+  }
+
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
